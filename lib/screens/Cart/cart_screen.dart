63820e88894cbc55ec3c4701a9dd14baf2953a67
Thibault Smt <thibault.schmitt@epitech.eu>
2022-11-09 20:33:53 +0100

    [REFACTOR]  the cart code

--- lib/screens/Cart/cart_screen.dart
+++ lib/screens/Cart/cart_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../constants/colors.dart';
 import '../../providers/produit_provider.dart';
+import 'Card/card_component.dart';
 
 class Cart extends StatefulWidget {
   const Cart({Key? key}) : super(key: key);
@@ -45,21 +45,21 @@ class _CartPage extends State<Cart> {
                   ),
                 ),
                 Expanded(
-                  child: Consumer<ProduitProvider>(
-                    builder: (
-                      BuildContext context,
-                      ProduitProvider productProv,
-                      Widget? child,
-                    ) =>
-                        ListView.builder(
-                      itemCount: selectedProduct.length,
-                      padding: const EdgeInsets.only(
-                        left: 10,
-                        right: 10,
-                        bottom: 10,
-                      ),
-                      itemBuilder: (BuildContext context, int index) {
-                        return Dismissible(
+                  child: ListView.builder(
+                    itemCount: selectedProduct.length,
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 10,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Consumer<ProduitProvider>(
+                        builder: (
+                          BuildContext context,
+                          ProduitProvider productProv,
+                          Widget? child,
+                        ) =>
+                            Dismissible(
                           direction: DismissDirection.endToStart,
                           key: Key(selectedProduct[index].id),
                           onDismissed: (DismissDirection direction) {
@@ -87,76 +87,12 @@ class _CartPage extends State<Cart> {
                               ),
                             ),
                           ),
-                          child: Card(
-                            elevation: 8,
-                            shadowColor: primaryColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: SizedBox(
-                              height: 120,
-                              child: Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 10,
-                                  right: 10,
-                                  bottom: 10,
-                                  top: 10,
-                                ),
-                                child: Row(
-                                  children: <Widget>[
-                                    Hero(
-                                      tag: selectedProduct[index].id,
-                                      child: Image.network(
-                                        productProv
-                                            .selectedProduct[index].picture,
-                                        width: 100,
-                                        height: 100,
-                                      ),
-                                    ),
-                                    const Divider(
-                                      height: 20,
-                                      thickness: 5,
-                                      indent: 20,
-                                      endIndent: 0,
-                                      color: Colors.black,
-                                    ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        const Spacer(),
-                                        Text(
-                                          productProv
-                                              .selectedProduct[index].name,
-                                        ),
-                                        const Text('this is an amazing phone'),
-                                        const Spacer(),
-                                        const Text('ref: ZFcse0984'),
-                                        Text(
-                                          'QTY: ${productProv.selectedProduct[index].id}',
-                                        ),
-                                        const Spacer(),
-                                      ],
-                                    ),
-                                    const Spacer(),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                        right: 10,
-                                        top: 10,
-                                        bottom: 10,
-                                      ),
-                                      child: Text(
-                                        '${productProv.selectedProduct[index].price} €',
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
+                          child: CardCart(
+                            product: productProv.selectedProduct[index],
                           ),
-                        );
-                      },
-                    ),
+                        ),
+                      );
+                    },
                   ),
                 ),
                 Padding(
