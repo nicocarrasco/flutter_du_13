4a3a749c09a89e29a17c38f7de7eaa748589c0bc
Thibault Smt <thibault.schmitt@epitech.eu>
2022-11-09 14:00:40 +0100

    [ADD] element in the card of the cart

--- lib/screens/Cart/cart_screen.dart
+++ lib/screens/Cart/cart_screen.dart
@@ -20,90 +20,106 @@ class Cart extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cart'),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 10, top: 10, bottom: 10),
-            child: Row(
+      body: selectedProduct.isEmpty
+          ? const Center(
+              child: Text("Your cart is empty", style: TextStyle(fontSize: 24)),
+            )
+          : Column(
               children: <Widget>[
-                if (selectedProduct.isNotEmpty)
-                  Text(
-                    '${selectedProduct.length} ${selectedProduct.length > 1 ? "items" : "item"}',
-                  )
-                else
-                  const Center(
-                    child: Text("Your cart is empty"),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        '${selectedProduct.length} ${selectedProduct.length > 1 ? "items" : "item"}',
+                      )
+                    ],
                   ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: selectedProduct.length,
-              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  elevation: 8,
-                  shadowColor: primaryColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: SizedBox(
-                    height: 120,
-                    child: Column(
-                      children: <Widget>[
-                        const Spacer(
-                          flex: 3,
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: selectedProduct.length,
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        elevation: 8,
+                        shadowColor: primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        Center(
-                          child: Hero(
-                            tag: 'test',
-                            child: Image.network(
-                              selectedProduct[index].picture,
-                              width: 100,
-                              height: 100,
+                        child: SizedBox(
+                          height: 120,
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              left: 10,
+                              right: 10,
+                              bottom: 10,
+                              top: 10,
+                            ),
+                            child: Row(
+                              children: <Widget>[
+                                Hero(
+                                  tag: selectedProduct[index].id,
+                                  child: Image.network(
+                                    selectedProduct[index].picture,
+                                    width: 100,
+                                    height: 100,
+                                  ),
+                                ),
+                                const Divider(
+                                  height: 20,
+                                  thickness: 5,
+                                  indent: 20,
+                                  endIndent: 0,
+                                  color: Colors.black,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    const Spacer(),
+                                    Text(selectedProduct[index].name),
+                                    const Text('this is an amazing phone'),
+                                    const Spacer(),
+                                    const Text('ref: ZFcse0984'),
+                                    Text('QTY: ${selectedProduct[index].id}'),
+                                    const Spacer(),
+                                  ],
+                                ),
+                                const Spacer(),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    right: 10,
+                                    top: 10,
+                                    bottom: 10,
+                                  ),
+                                  child:
+                                      Text('${selectedProduct[index].price} €'),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                        const Spacer(
-                          flex: 3,
-                        ),
-                        Row(
-                          children: const <Widget>[
-                            Spacer(),
-                            Text('test'),
-                            Spacer(
-                              flex: 2,
-                            ),
-                            Text('${10} €'),
-                            Spacer()
-                          ],
-                        ),
-                        const Spacer(
-                          flex: 2,
-                        ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 20),
-            child: Row(
-              children: <Widget>[
-                const Spacer(),
-                Text(
-                  selectedProduct.isNotEmpty
-                      ? 'Total: ${selectedProduct.map((Product e) => e.price).reduce((int value, int element) => value + element)} €'
-                      : '0€',
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(right: 20, top: 10, bottom: 20),
+                  child: Row(
+                    children: <Widget>[
+                      const Spacer(),
+                      Text(
+                        selectedProduct.isNotEmpty
+                            ? 'Total: ${selectedProduct.map((Product e) => e.price).reduce((int value, int element) => value + element)} €'
+                            : '0€',
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
