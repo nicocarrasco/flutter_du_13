4109d56a46cc6c4a8fab12f854353eb300d14d72
Thibault Smt <thibault.schmitt@epitech.eu>
2022-11-09 17:52:54 +0100

    [ADD] The dismiss effect on the card form the cart

--- lib/screens/Cart/cart_screen.dart
+++ lib/screens/Cart/cart_screen.dart
@@ -4,10 +4,18 @@ import 'package:provider/provider.dart';
 import '../../constants/colors.dart';
 import '../../providers/produit_provider.dart';
 
-class Cart extends StatelessWidget {
-  const Cart({
-    super.key,
-  });
+class Cart extends StatefulWidget {
+  const Cart({Key? key}) : super(key: key);
+
+  @override
+  State<Cart> createState() => _CartPage();
+}
+
+class _CartPage extends State<Cart> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,71 +45,106 @@ class Cart extends StatelessWidget {
                   ),
                 ),
                 Expanded(
-                  child: ListView.builder(
-                    itemCount: selectedProduct.length,
-                    padding:
-                        const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        elevation: 8,
-                        shadowColor: primaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: SizedBox(
-                          height: 120,
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              left: 10,
-                              right: 10,
-                              bottom: 10,
-                              top: 10,
+                  child: Consumer<ProduitProvider>(
+                    builder: (
+                      BuildContext context,
+                      ProduitProvider productProv,
+                      Widget? child,
+                    ) =>
+                        ListView.builder(
+                      itemCount: selectedProduct.length,
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                        bottom: 10,
+                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Dismissible(
+                          key: Key(selectedProduct[index].id),
+                          onDismissed: (DismissDirection direction) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  '${productProv.selectedProduct[index].name} dismissed',
+                                ),
+                              ),
+                            );
+                            setState(() {
+                              productProv.removeProduct(index);
+                            });
+                          },
+                          // Show a red background as the item is swiped away.
+                          background: Container(color: Colors.red),
+                          child: Card(
+                            elevation: 8,
+                            shadowColor: primaryColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                            child: Row(
-                              children: <Widget>[
-                                Hero(
-                                  tag: selectedProduct[index].id,
-                                  child: Image.network(
-                                    selectedProduct[index].picture,
-                                    width: 100,
-                                    height: 100,
-                                  ),
+                            child: SizedBox(
+                              height: 120,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 10,
+                                  right: 10,
+                                  bottom: 10,
+                                  top: 10,
                                 ),
-                                const Divider(
-                                  height: 20,
-                                  thickness: 5,
-                                  indent: 20,
-                                  endIndent: 0,
-                                  color: Colors.black,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                child: Row(
                                   children: <Widget>[
+                                    Hero(
+                                      tag: selectedProduct[index].id,
+                                      child: Image.network(
+                                        productProv
+                                            .selectedProduct[index].picture,
+                                        width: 100,
+                                        height: 100,
+                                      ),
+                                    ),
+                                    const Divider(
+                                      height: 20,
+                                      thickness: 5,
+                                      indent: 20,
+                                      endIndent: 0,
+                                      color: Colors.black,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        const Spacer(),
+                                        Text(
+                                          productProv
+                                              .selectedProduct[index].name,
+                                        ),
+                                        const Text('this is an amazing phone'),
+                                        const Spacer(),
+                                        const Text('ref: ZFcse0984'),
+                                        Text(
+                                          'QTY: ${productProv.selectedProduct[index].id}',
+                                        ),
+                                        const Spacer(),
+                                      ],
+                                    ),
                                     const Spacer(),
-                                    Text(selectedProduct[index].name),
-                                    const Text('this is an amazing phone'),
-                                    const Spacer(),
-                                    const Text('ref: ZFcse0984'),
-                                    Text('QTY: ${selectedProduct[index].id}'),
-                                    const Spacer(),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                        right: 10,
+                                        top: 10,
+                                        bottom: 10,
+                                      ),
+                                      child: Text(
+                                        '${productProv.selectedProduct[index].price} €',
+                                      ),
+                                    ),
                                   ],
                                 ),
-                                const Spacer(),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                    right: 10,
-                                    top: 10,
-                                    bottom: 10,
-                                  ),
-                                  child:
-                                      Text('${selectedProduct[index].price} €'),
-                                ),
-                              ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
                 ),
                 Padding(
